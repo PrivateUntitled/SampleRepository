1366226bba13519d918692bd194fe4c67e6a4cf3
PublicTitled <home@Magic-Baguette.local>
2026-01-15 22:07:44 +0800

    edited sample script

--- My project/Assets/SampleScript.cs
+++ My project/Assets/SampleScript.cs
@@ -7,7 +7,7 @@ public class SampleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("test");
     }
 
     // Update is called once per frame
